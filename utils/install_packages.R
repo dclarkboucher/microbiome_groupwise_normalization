520d8f86eb5d4e8242eeeb64305c1ce2c8730028
Dylan Clark-Boucher <clarkbou@umich.edu>
2025-06-29 12:17:06 -0400

    update readme

--- utils/install_packages.R
+++ utils/install_packages.R
@@ -1,5 +1,5 @@
 install.packages(c("tibble", "dplyr", "MicrobiomeStat", "LaplacesDemon",
-                   "mvtnorm"))
+                   "mvtnorm", "tidyr", "forcats", "ggpubr"))
 install.packages("BiocManager") # need this for installing other packages
 
 BiocManager::install(c("edgeR", "DESeq2","metagenomeSeq"))
